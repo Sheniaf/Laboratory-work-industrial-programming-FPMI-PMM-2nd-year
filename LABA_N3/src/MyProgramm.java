23413591a06d80b3ce93fb94411101ec8b4fd15b
даниил <danya.danlovich@yandex.ru>
2022-11-30 20:26:39 +0300

    f

--- LABA_N3/src/MyProgramm.java
+++ LABA_N3/src/MyProgramm.java
@@ -1,6 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 
-public class MyProgramm {
-
+public class MyProgramm{
+    public static void main(String[] args) throws IOException {
+        ArrayList <String> words = new ArrayList<String>();
+        File file = new File("LABA_N3/src/input");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+        String line = null;
+        while((line = reader.readLine()) != null){
+            String[] words_t = line.split(" ");
+            int t = words_t.length;
+            for(int i = 0; i < t; i++)
+                words.add(words_t[i]);
+            line = null;
+        }
+        String regex = "[0-9]+";
+        for(int i = 0; i < words.size(); i++){
+            if(!words.get(i).matches(regex)){
+                System.out.print(words.get(i) + " ");
+            }
+        }
+    }
 }
